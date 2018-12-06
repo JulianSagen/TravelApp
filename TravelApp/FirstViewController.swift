6ea08bdd4c3bfd7422495a38684f42d48b4294be
DMSIMAC02 <DMSIMAC02@dmsimac02.local>
2018-12-06 13:08:51 -0800

    Initial Commit

---
+++ TravelApp/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  TravelApp
+//
+//  Created by DMSIMAC02 on 12/6/18.
+//  Copyright © 2018 DMSMBP-001. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
